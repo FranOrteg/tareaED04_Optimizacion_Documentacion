a38de762acd14a24671fbe1a6084169a51b3576d
Fran Ortega <ortegfran@gmail.com>
2024-03-03 12:01:31 +0100

    6. Insertar comentarios Javadoc

--- src/cuentas/CCuenta.java
+++ src/cuentas/CCuenta.java
@@ -1,7 +1,17 @@
 package cuentas;
 
-public class CCuenta {
+/**
+ * Esta clase define un tipo de cuenta bancaria
+ * @author Francisco Ortega
+ * @version 1.0
+ * @since 2/03/2024
+ * @see <a href = https://github.com/FranOrteg/tareaED04_Optimizacion_Documentacion.git>Repositorio Tarea04</a>
+ */
 
+
+public class CCuenta {
+	
+	// Atributos de la Clase
 
     private String nombre;
     private String cuenta;
@@ -11,18 +21,33 @@ public class CCuenta {
     public CCuenta()
     {
     }
+    
+    // Constructor de la clase que recibe métodos setters
 
     public CCuenta(String nom, String cue, double sal, double tipo)
     {
         setNombre(nom);
         setCuenta(cue);
         setSaldo(sal);
+        setTipoInterés(tipo);
     }
+    
+    /**
+     * Método 
+     * @return saldo Devuelve el saldo de la cuenta
+     */
 
     public double estado()
     {
         return getSaldo();
     }
+    
+    // Método para ingresar una cantidad en la cuenta, tendrá que ser positiva para no lanzar una excepción.
+    
+    /**
+     * @param cantidad La cantidad a ingresar
+     * @throws Exception Debe ser un numero positivo
+     */
 
     public void ingresar(double cantidad) throws Exception
     {
@@ -30,6 +55,14 @@ public class CCuenta {
             throw new Exception("No se puede ingresar una cantidad negativa");
         setSaldo(getSaldo() + cantidad);
     }
+    
+    /**
+     * Método para retirar una cantidad de la cuenta, está será positiva para no lanzar la primera excepción,
+     * y mayor que el estado para no lanzar la segunda excepción.
+     * 
+     * @param cantidad La cantidad a retirar
+     * @throws Exception Debe ser un numero positivo y deberia ser menor que el saldo
+     */
 
     public void retirar(double cantidad) throws Exception
     {
@@ -39,34 +72,75 @@ public class CCuenta {
             throw new Exception ("No se hay suficiente saldo");
         setSaldo(getSaldo() - cantidad);
     }
+    
+    // METODOS SETTERS Y GETTERS DE LOS ATRIBUTOS DE CLASE
+    
+    /**
+     * @return nombre Devuelve el nombre
+     */
 
 	private String getNombre() {
 		return nombre;
 	}
+	
+	/**
+	 * 
+	 * @param nombre Establece el nombre
+	 */
 
 	private void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
+	/**
+	 * 
+	 * @return cuenta Devuelve la cuenta
+	 */
 
 	private String getCuenta() {
 		return cuenta;
 	}
+	
+	/**
+	 * 
+	 * @param cuenta Establece el valor de la cuenta
+	 */
 
 	private void setCuenta(String cuenta) {
 		this.cuenta = cuenta;
 	}
+	
+	/**
+	 * 
+	 * @return saldo Devuelve el saldo
+	 */
 
 	private double getSaldo() {
 		return saldo;
 	}
+	
+	/**
+	 * 
+	 * @param saldo Establece el saldo de la cuenta
+	 */
 
 	private void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
 
+	/**
+	 * 
+	 * @return TipoInterés Devuelve el tipo de interés
+	 */
+	
 	private double getTipoInterés() {
 		return tipoInterés;
 	}
+	
+	/**
+	 * 
+	 * @param tipoInterés Establece el tipo de interés
+	 */
 
 	private void setTipoInterés(double tipoInterés) {
 		this.tipoInterés = tipoInterés;
